6fbe962972026bddebe90c84e2dfbf5e92ad9d3e
CsonkaDavid <shay9505@gmail.com>
2022-11-07 01:31:04 +0100

    Titles file created to handle page titles here

---
+++ TRFM-tutorial-site/src/components/Titles.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+
+export function MainPageTitle() {
+  return (
+    useEffect(() => {
+        document.title = 'TRFM-Main';
+    })
+  );
+}
+
+export default MainPageTitle
