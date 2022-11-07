d620ecb284c7a12dda69777dbf5001d4ee6d53f9
CsonkaDavid <shay9505@gmail.com>
2022-11-07 01:32:27 +0100

    Rewrote and redesigned MainPage intro text

--- TRFM-tutorial-site/src/components/MainPageIntro.jsx
+++ TRFM-tutorial-site/src/components/MainPageIntro.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
-import {FaReact} from 'react-icons/fa';
-import {SiTailwindcss, SiFramer} from 'react-icons/si';
+import {motion} from 'framer-motion'
+import './design/MainPageIntro.css'
 
 function MainPageIntro() {
   return (
-    <div>
-        <p>
-            This website was made with the following technologies:
-            </p>
-                <p className='flex'>
-                    <div className='my-auto mr-3 text-cyan-400'>
-                        <FaReact size={40}/>
-                    </div>
-                    <div>
-                        React JS 
-                    </div> 
+        <div className='text-4xl font-bold w-[30em] h-fit mt-[3.5em]'>
+            <motion.div
+                initial={{ opacity: 0, scale: 1 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{duration: 1, delay: 1.2, ease: [0.2, 0.8, 0.4, 0.9]}}>
+
+                <p className='mb-[3em]'>
+                    This website was made as a "self-teaching learning" project about frontend development.
                 </p>
-                <p className='flex'>
-                    <div className='my-auto mr-3 text-blue-400'>
-                        <SiTailwindcss size={40}/>
-                    </div>
-                    <div>
-                        Tailwind CSS 
-                    </div> 
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, scale: 1 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{duration: 1, delay: 2, ease: [0.2, 0.8, 0.4, 0.9]}}>
+
+                <p className='mb-[3em]'>
+                    The goal was to grasp the basics of many different frameworks used in the making of modern frontend applications.
                 </p>
-                <p className='flex'>
-                    <div className='my-auto mr-3 text-purple-400'>
-                        <SiFramer size={40}/>
-                    </div>
-                    <div>
-                        Framer Motion 
-                    </div> 
-            </p>
-    </div>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0, scale: 1 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{duration: 1, delay: 2.8, ease: [0.2, 0.8, 0.4, 0.9]}}>
+
+                <p>
+                    The first result with a public repository is this website.
+                </p>
+            </motion.div>
+        </div>   
   )
 }
 
